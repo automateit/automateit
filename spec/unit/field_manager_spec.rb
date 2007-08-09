2fd220d7a9c2552529c13bc3de28f29b35ef8351
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-09 07:23:50 -0700

    Removed obsoleted comments

--- spec/unit/field_manager_spec.rb
+++ spec/unit/field_manager_spec.rb
@@ -19,25 +19,11 @@ describe "AutomateIt::FieldManager", :shared => true do
   end
 
   it "should have aliased lookup into the interpreter" do
-    #require 'rubygems'; require 'breakpoint'; breakpoint # FIXME 
-    #@m.interpreter.lookup("hash#branchkey#deepleafkey").should eql("deepleafvalue")
-=begin
-irb(#<#<Class:0xb7681840>:0xb7673f9c>):004:0>  @m.interpreter.field_manager[:yaml]
-=> #<AutomateIt::FieldManager::YAML:0xb76734c0>
-irb(#<#<Class:0xb7681840>:0xb7673f9c>):005:0>  @m[:yaml]
-=> #<AutomateIt::FieldManager::YAML:0xb76720ac @struct={"hash"=>{"leafkey"=>"leafvalue", "branchkey"=>{"deepleafkey"=>"deepleafvalue"}}, "key"=>"value"}>
-
-irb(#<#<Class:0xb7681840>:0xb7673f9c>):010:0> @m.object_id
-=> -608985708
-irb(#<#<Class:0xb7681840>:0xb7673f9c>):011:0> @m.interpreter.field_manager.object_id
-=> -608985928
-=end
     @m.object_id.should == @m.interpreter.field_manager.object_id
     @m[:yaml].object_id.should == @m.interpreter.field_manager[:yaml].object_id
   end
 end
 
-# FIXME use @m.setup(:default => :struct, :struct => {...})
 describe AutomateIt::FieldManager::Struct do
   it_should_behave_like "AutomateIt::FieldManager"
 
