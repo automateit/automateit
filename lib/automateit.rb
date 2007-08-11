5bcff1d060849503e47a97deab0e2f2a50b2e570
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-11 06:43:40 -0700

    Extracted FieldManager to own file.

--- lib/automateit.rb
+++ lib/automateit.rb
@@ -301,63 +301,11 @@ module AutomateIt #:main: AutomateIt
         return -1
       end
     end
-
   end
-
-  class FieldManager < Plugin::Manager
-    alias_methods :lookup
-
-    def lookup(search) dispatch(search) end
-
-    class Struct < Plugin::Driver
-      def suitability(method, *args)
-        return 1
-      end
-
-      def setup(opts={})
-        super(opts)
-
-        if opts[:struct]
-          @struct = opts[:struct] 
-        else
-          @struct = {}
-        end
-      end
-
-      def lookup(search)
-        ref = @struct
-        for key in search.to_s.split("#")
-          ref = ref[key]
-        end
-        ref
-      end
-    end
-
-    require 'erb'
-    require 'yaml'
-    class YAML < Struct
-      def suitability(method, *args)
-        return 5
-      end
-
-      def setup(opts={})
-        if filename = opts.delete(:file)
-          opts[:struct] = ::YAML::load(ERB.new(_read(filename), nil, '-').result)
-        end
-        super(opts)
-      end
-
-      def _read(filename)
-        return File.read(filename)
-      end
-
-    end
-
-  end
-
 end
 
 require 'automateit/address_manager'
+require 'automateit/field_manager'
 require 'automateit/platform_manager'
 require 'automateit/shell_manager'
 require 'automateit/tag_manager'
---
+++ lib/automateit/field_manager.rb
@@ -0,0 +1,54 @@
+require 'automateit'
+
+module AutomateIt
+  class FieldManager < Plugin::Manager
+    alias_methods :lookup
+
+    def lookup(search) dispatch(search) end
+
+    class Struct < Plugin::Driver
+      def suitability(method, *args)
+        return 1
+      end
+
+      def setup(opts={})
+        super(opts)
+
+        if opts[:struct]
+          @struct = opts[:struct]
+        else
+          @struct = {}
+        end
+      end
+
+      def lookup(search)
+        ref = @struct
+        for key in search.to_s.split("#")
+          ref = ref[key]
+        end
+        ref
+      end
+    end
+
+    require 'erb'
+    require 'yaml'
+    class YAML < Struct
+      def suitability(method, *args)
+        return 5
+      end
+
+      def setup(opts={})
+        if filename = opts.delete(:file)
+          opts[:struct] = ::YAML::load(ERB.new(_read(filename), nil, '-').result)
+        end
+        super(opts)
+      end
+
+      def _read(filename)
+        return File.read(filename)
+      end
+
+    end
+
+  end
+end
