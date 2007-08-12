508bc4d813e4bce6628d9a76d7cde1069904ad93
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-12 13:54:07 -0700

    Commonized logic that generates the ip commands and added aliases for commonly misnamed options.

--- lib/automateit/address_manager.rb
+++ lib/automateit/address_manager.rb
@@ -79,12 +79,8 @@ module AutomateIt
         raise SecurityEror.new("you must be root") unless Process.euid.zero?
         raise ArgumentError.new(":device and :address must be specified") unless opts[:device] and opts[:address]
         return false if has?(opts)
-        ipcmd = "ip address add #{opts[:address]}"
-        ipcmd += "/#{opts[:mask]}" if opts[:mask]
-        ipcmd += " brd + dev #{opts[:device]}"
-        ipcmd += " label #{opts[:device]}:#{opts[:label]}" if opts[:label]
         #run(%{ip address add #{ip}/#{mask} brd + dev #{device} label #{device}:#{label}})
-        if interpreter.sh(ipcmd)
+        if interpreter.sh(_add_or_remove_command(:add, opts))
           #run(%{arping -q -c 3 -A -I #{device} #{ip} &})
           return interpreter.sh("arping -q -c #{announcements} -A -I #{opts[:device]} #{opts[:address]}")
         else
@@ -96,12 +92,31 @@ module AutomateIt
         return false unless has?(opts)
         raise SecurityEror.new("you must be root") unless Process.euid.zero?
         raise ArgumentError.new(":device and :address must be specified") unless opts[:device] and opts[:address]
-        ipcmd = "ip address del #{opts[:address]}"
+        return interpreter.sh(_add_or_remove_command(:remove, opts))
+      end
+
+      def _add_or_remove_command(action, opts)
+        case action.to_sym
+        when :add
+          # Accept
+        when :remove
+          # Rename
+          action = :del
+        else
+          raise ArgumentError.new("action must be :add or :remove")
+        end
+
+        # Accept common alternative names
+        opts[:mask] ||= opts[:netmask] if opts[:netmask]
+        opts[:alias] ||= opts[:alias] if opts[:alias]
+        opts[:device] ||= opts[:interface] if opts[:interface]
+
+        # ip address add 10.0.0.123/24 brd + dev eth0 label eth0:foo
+        ipcmd = "ip address #{action} #{opts[:address]}"
         ipcmd += "/#{opts[:mask]}" if opts[:mask]
         ipcmd += " brd + dev #{opts[:device]}"
         ipcmd += " label #{opts[:device]}:#{opts[:label]}" if opts[:label]
-        #run(%{ip address del #{ip}/#{mask} brd + dev #{device} label #{device}:#{label}})
-        return interpreter.sh(ipcmd)
+        return ipcmd
       end
 
       def interfaces()
