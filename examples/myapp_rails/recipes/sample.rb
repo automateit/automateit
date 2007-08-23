8a815a8f3c5a73d107211e2105b47cd66d37214b
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-23 16:57:18 -0700

    Added myapp_rails example code.

---
+++ examples/myapp_rails/recipes/sample.rb
@@ -0,0 +1,80 @@
+# Setup Rails on hosts tagged "rails_servers" or "myapp_servers"
+if tagged?("rails_servers | myapp_servers")
+  # Install system-level packages, if needed
+  if tagged?("ubuntu | debian")
+    package_manager.install(%w(build-essential swig ruby1.8-dev libsqlite3-dev pen))
+  elsif tagged?("fedoracore | redhat | centos")
+    package_manager.install(%w(ruby-devel sqlite-devel pen))
+  else
+    raise NotImplementedError.new("no packages specified for this platform")
+  end
+
+  # Install Ruby gems, if needed
+  package_manager.install(%w(rails mongrel mongrel_cluster sqlite3-ruby), :with => :gem)
+end
+
+# Install myapp app
+if tagged?("myapp_servers")
+  # Create a user login, if needed
+  account_manager.add_user(lookup("myapp#user"))
+  mkdir_p(lookup("myapp#path")) do
+    # Track changes so we know when to restart the Rails server
+    restart_needed= false
+
+    # Create Rails application, if needed
+    sh("rails --database=sqlite3 .") unless File.exists?("config/routes.rb")
+
+    # Create mongrel cluster configuration from template, if needed
+    render(
+      :string => \
+"<%=WARNING_BOILERPLATE%>
+
+cwd: <%=path%>
+port: <%=port%>
+environment: <%=environment%>
+pid_file: <%=path%>/log/mongrel.pid
+servers: <%=backends%>",
+      :to => "config/mongrel_cluster.yml",
+      :locals => {
+        :environment => lookup("myapp#environment"),
+        :path => lookup("myapp#path"),
+        :port => lookup("myapp#port"),
+        :backends => lookup("myapp#backends")
+      }
+    )
+
+    # Instantiate db, if needed
+    sh "rake db:migrate" if Dir["db/*.sqlite3"].empty?
+
+    # Edit a file, if needed
+    restart_needed |= edit(
+      :file => "public/index.html", :params => {:name => lookup("myapp#name")}
+    ) do
+      append("<!-- Edited by AutomateIt -->")
+      replace("Welcome aboard", "This is "+params[:name])
+    end
+
+    # Create service, if needed
+    restart_needed |= render(
+      :file => dist+"/etc/init.d/"+lookup("myapp#name")+".erb",
+      :to => "/etc/init.d/"+lookup("myapp#name"),
+      :locals => {
+        :user => lookup("myapp#user"),
+        :path => lookup("myapp#path"),
+        :port => lookup("myapp#port"),
+        :backends => lookup("myapp#backends")
+      }
+    )
+
+    # Enable service to run on boot, if needed
+    service_manager.enable(lookup("myapp#name"))
+
+    # Start or restart service, if needed
+    if restart_needed
+      service_manager.restart(lookup("myapp#name"))
+    else
+      service_manager.start(lookup("myapp#name"))
+    end
+  end
+end
+
