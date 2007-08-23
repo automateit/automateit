2123740e34457e9e08fd897bc8c3c94ba4261fa9
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-23 16:51:54 -0700

    Implemented primive AccountManager that only does add_user for sake of example code.

--- lib/automateit/account_manager.rb
+++ lib/automateit/account_manager.rb
@@ -16,3 +16,42 @@ groups[id or name] returns same fields as OPTIONS
 add_users_to_group(group id or name, [names or ids of users]) [or with def order]
 add_users_to_group([names or ids of users], group id or name)
 =end
+
+module AutomateIt
+  class AccountManager < Plugin::Manager
+
+    # TODO implement remaining methods
+
+    def add_user(username, opts={}) dispatch(username, opts) end
+
+    #-----------------------------------------------------------------------
+
+    class Basic < Plugin::Driver
+      depends_on :nothing
+
+      def suitability(method, *args)
+        return 1
+      end
+    end # class Basic
+
+    #-----------------------------------------------------------------------
+
+    class POSIX < Basic
+      depends_on :programs => %w(useradd usermod userdel groupadd groupmod groupdel)
+
+      def suitability(method, *args)
+        return available? ? 2 : 0
+      end
+
+      def add_user(username, opts={})
+        begin
+          Etc.getpwnam(username)
+          return true
+        rescue ArgumentError
+          # TODO pass more arguments
+          interpreter.sh("useradd --create-home #{username}")
+        end
+      end
+    end # class POSIX
+  end # class AccountManager
+end # module AutomateIt
