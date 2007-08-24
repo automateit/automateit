264997fbffad79c4e3e123d6ab3f7d625d072532
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-23 18:28:32 -0700

    Fixed assorted typos, added chown in exampl.

--- examples/myapp_rails/recipes/sample.rb
+++ examples/myapp_rails/recipes/sample.rb
@@ -18,11 +18,13 @@ if tagged?("myapp_servers")
   # Create a user login, if needed
   account_manager.add_user(lookup("myapp#user"))
   mkdir_p(lookup("myapp#path")) do
+    chown(lookup("myapp#user"), nil, ".")
+
     # Track changes so we know when to restart the Rails server
     restart_needed= false
 
     # Create Rails application, if needed
-    sh("rails --database=sqlite3 .") unless File.exists?("config/routes.rb")
+    sh("su #{lookup('myapp#user')} -c 'rails --database=sqlite3 .'") unless File.exists?("config/routes.rb")
 
     # Create mongrel cluster configuration from template string, if needed
     render(
@@ -39,12 +41,12 @@ servers: <%=backends%>",
         :environment => lookup("myapp#environment"),
         :path => lookup("myapp#path"),
         :port => lookup("myapp#port"),
-        :backends => lookup("myapp#backends")
+        :backends => lookup("myapp#backends"),
       }
     )
 
     # Instantiate db, if needed
-    sh "rake db:migrate" if Dir["db/*.sqlite3"].empty?
+    sh "su #{lookup('myapp#user')} -c 'rake db:migrate'" if Dir["db/*.sqlite3"].empty?
 
     # Edit a file to set the application name, if needed
     restart_needed |= edit(
@@ -55,15 +57,18 @@ servers: <%=backends%>",
     end
 
     # Create service by rendering a template file, if needed
+    # FIXME dryrun isn't showing this file being rendered
     restart_needed |= render(
       :file => dist+"/etc/init.d/"+lookup("myapp#name")+".erb",
       :to => "/etc/init.d/"+lookup("myapp#name"),
       :mode => 0555,
       :locals => {
+        :name => lookup("myapp#name"),
         :user => lookup("myapp#user"),
         :path => lookup("myapp#path"),
+        :bind => lookup("myapp#bind"),
         :port => lookup("myapp#port"),
-        :backends => lookup("myapp#backends")
+        :backends => lookup("myapp#backends"),
       }
     )
 
@@ -71,8 +76,9 @@ servers: <%=backends%>",
     service_manager.enable(lookup("myapp#name"))
 
     # Start or restart service, if needed
-    if restart_needed
-      service_manager.restart(lookup("myapp#name"))
+    if service_manager.running?(lookup("myapp#name")) and restart_needed
+      # FIXME it's always restarting it even if it's not running
+      service_manager.tell(lookup("myapp#name"), "restart")
     else
       service_manager.start(lookup("myapp#name"))
     end
