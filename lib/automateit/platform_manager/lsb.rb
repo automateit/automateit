04f54f8868694e584d839d4ca73a4af890956dfc
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-26 19:54:37 -0700

    Refactored PlatformManager::LSB to be much faster without any caching by calling lsb_release with explicit arguments

--- lib/automateit/platform_manager/lsb.rb
+++ lib/automateit/platform_manager/lsb.rb
@@ -20,8 +20,7 @@ module AutomateIt
         @struct[:distro]  ||= @@struct_cache[:distro]
         @struct[:release] ||= @@struct_cache[:release]
         unless @struct[:distro] and @struct[:release]
-          # TODO Refactor using lazy-loading to boost performance.
-          data = _read_lsb_release_output # SLOW 1.3s
+          data = _read_lsb_release_output # SLOW 0.2s
           begin
             yaml = YAML::load(data)
             @struct[:distro]  ||= @@struct_cache[:distro]  ||= yaml["Distributor ID"].to_s.downcase
@@ -33,9 +32,9 @@ module AutomateIt
       end
 
       def _read_lsb_release_output
-        # Discard STDERR because "lsb_release" spews warnings like "No LSB
-        # modules are available." that we don't care about.
-        return `"#{LSB_RELEASE}" -a 2>/dev/null`.gsub(/\t/, " ")
+        # Do NOT use 'lsb_release -a' because this takes a few seconds. Telling
+        # 'lsb_release' which fields we want makes it much faster.
+        return `"#{LSB_RELEASE}" --release --id`.gsub(/\t/, " ")
       end
       private :_read_lsb_release_output
     end
