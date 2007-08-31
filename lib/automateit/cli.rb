746ed6e83f157bbf236bac9603fb957647e41c81
Igal Koshevoy <igal+hg@koshevoy.com>
2007-08-30 23:36:33 -0700

    Added auto-completion of commands to interactive shell.

--- lib/automateit/cli.rb
+++ lib/automateit/cli.rb
@@ -25,22 +25,38 @@ module AutomateIt
         AutomateIt.invoke(opts[:recipe], opts)
       else
         require "irb"
+
+        # Welcome messages
         unless opts[:quiet]
           puts PNOTE+"AutomateIt Shell v#{AutomateIt::VERSION}"
           puts PNOTE+"<CTRL-D> to quit, <Tab> to auto-complete"
         end
+
+        # Create and connect instances
         IRB.setup(__FILE__)
         irb = IRB::Irb.new
         opts[:irb] = irb
         IRB.conf[:MAIN_CONTEXT] = irb.context
         interpreter = AutomateIt.new(opts)
         irb.context.workspace.instance_variable_set(:@binding, interpreter.send(:binding))
+
+        # Tab completion
+        require 'irb/completion'
+        irb.context.auto_indent_mode = true
+        irb.context.use_readline = true
+        unless irb.context.load_modules.include?('irb/completion')
+          irb.context.load_modules << 'irb/completion'
+        end
+
+        # Set prompt
         unless opts[:custom_prompt] == false
           irb.context.prompt_i = "ai> "
           irb.context.prompt_s = "ai%l "
           irb.context.prompt_c = "ai* "
           irb.context.prompt_n = "ai%i "
         end
+
+        # Run loop to read user input
         irb.eval_input
       end
     end
