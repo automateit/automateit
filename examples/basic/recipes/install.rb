c0b154911cc87445915b7d8499dbe308063fa4ee
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-03 17:03:37 -0700

    Added parens to example recipe for clarity.

--- examples/basic/recipes/install.rb
+++ examples/basic/recipes/install.rb
@@ -1,40 +1,40 @@
-# Install packages
-if tagged? "rails_servers | myapp_servers"
-  # Install platform-specific packages
-  if tagged? "ubuntu | debian"
-    package_manager.install %w(ruby1.8-dev libsqlite3-dev)
-  elsif tagged? "fedoracore | redhat | centos"
-    package_manager.install %w(ruby-devel sqlite-devel)
+# Install packages on machines tagged 'rails_servers' or 'myapp_servers'
+if tagged?("rails_servers | myapp_servers")
+  # Install platform-specific packages, queries system-provided tags
+  if tagged?("ubuntu | debian")
+    package_manager.install(%w(ruby1.8-dev libsqlite3-dev))
+  elsif tagged?("fedoracore")
+    package_manager.install(%w(ruby-devel sqlite-devel))
   else
     raise NotImplementedError.new("no packages specified for this platform")
   end
 
   # Install Gems
-  package_manager.install %w(rails sqlite3-ruby mongrel),
-    :with => :gem, :docs => false
+  package_manager.install(%w(rails sqlite3-ruby mongrel),
+    :with => :gem, :docs => false)
 end
 
 # Setup the myapp server
-if tagged? :myapp_servers
+if tagged?(:myapp_servers)
   # Create a directory for the application
-  mkdir_p lookup(:path) do
+  mkdir_p(lookup(:path)) do
     # Run shell commands to create the app and database
-    sh "rails --database=sqlite3 . > /dev/null" \
+    sh("rails --database=sqlite3 . > /dev/null") \
       unless File.exists?("config/routes.rb")
 
-    sh "rake db:migrate" if Dir["db/*.sqlite3"].empty?
+    sh("rake db:migrate") if Dir["db/*.sqlite3"].empty?
 
     # Edit the homepage
-    edit :file => "public/index.html" do
-      append "<!-- Edited by AutomateIt -->"
-      replace "Welcome aboard", "This is MyAppServer"
+    edit(:file => "public/index.html") do
+      append("<!-- Edited by AutomateIt -->")
+      replace("Welcome aboard", "This is MyAppServer")
     end
 
     # Change the ownership for the files
-    chown_R lookup(:user), nil, "."
+    chown_R(lookup(:user), nil, ".")
 
     # Generate a service startup file from a template
-    render :file => dist+"myapp_server.erb",
+    render(:file => dist+"myapp_server.erb",
            :to => "/etc/init.d/myapp_server",
            :mode => 0555,
            :locals => {
@@ -42,8 +42,9 @@ if tagged? :myapp_servers
               :user => lookup(:user),
               :port => lookup(:port),
            }
+    )
 
     # Start the service
-    service_manager.start "myapp_server"
+    service_manager.start("myapp_server")
   end
 end
