9ef2508caf37f0a62bab925936a7870fafc9c149
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-12 17:53:54 -0700

    Figured out how to run the examples silently, although the implementations are problematic.

--- spec/integration/examples_spec.rb
+++ spec/integration/examples_spec.rb
@@ -1,52 +1,47 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), "/../spec_helper.rb")
 
-# Good way to run this to confirm the startup sequence works flawlessly -- this leaves the server running, stops it, and confirms that the :wait works:
-#  spec -e "should install the example" spec/integration/examples_spec.rb; /etc/init.d/myapp_server stop; spec -e "should install the example" spec/integration/examples_spec.rb
+# This spec runs the ruby interpreter directly. This makes it possible to silently run the recipes, without editing them. Unfortunately, this is evil and brittle because it relies on being able to run the interpreter again. The alternative is "spec/integration/examples_spec_editor.rb", which edits the recipes so they're silent and uses the internal invoke method to run them. What's a reasonable way to do this?
 
 if not INTERPRETER.superuser?
   puts "NOTE: Must be root to check #{__FILE__}"
 else
   describe "Examples" do
-    project = "examples/basic"
-    installer = "examples/basic/recipes/install.rb"
-    uninstaller = "examples/basic/recipes/uninstall.rb"
+    params = {
+      :project => "examples/basic",
+      :installer => "examples/basic/recipes/install.rb",
+      :uninstaller => "examples/basic/recipes/uninstall.rb",
+    }
+    INTERPRETER.params = params
+
+    # Get the fully qualified filename for the interpreter.
+    # XXX What bad things will this do if, say, running with JRuby?
+    ruby = begin
+      c = ::Config::CONFIG
+      File::join(c['bindir'], c['ruby_install_name']) << c['EXEEXT']
+    end
 
     begin
       # This will throw a NotImplemented error on unsupported platforms
-      AutomateIt::invoke(installer, :verbosity => Logger::WARN, :noop => true)
+      AutomateIt::invoke(params[:installer], :verbosity => Logger::WARN, :noop => true)
 
       it "should install the example" do
-        INTERPRETER.mktemp do |recipe|
-          INTERPRETER.log.silence(Logger::WARN) do
-            INTERPRETER.cp(installer, recipe)
-            INTERPRETER.edit(:file => recipe) do
-              replace 'service_manager.start\("myapp_server"\)$',
-                "service_manager.start 'myapp_server', :silent => true"
-            end or raise "couldn't edit installer"
-            ### INTERPRETER.cp recipe, "/tmp/installer.rb"
-
-            AutomateIt::invoke(recipe, :project => project, :verbosity => Logger::WARN)
+        INTERPRETER.instance_eval do
+          log.silence(Logger::WARN) do
+            sh("#{ruby} bin/automateit #{params[:installer]} > /dev/null 2>&1")
             File.exists?("/etc/init.d/myapp_server").should be_true
             File.directory?("/tmp/myapp_server").should be_true
-            INTERPRETER.service_manager.started?("myapp_server", :wait => 5).should be_true
+            service_manager.started?("myapp_server", :wait => 5).should be_true
           end
         end
       end
 
       it "should uninstall the example" do
-        INTERPRETER.mktemp do |recipe|
-          INTERPRETER.log.silence(Logger::WARN) do
-            INTERPRETER.cp(uninstaller, recipe)
-            INTERPRETER.edit(:file => recipe) do
-              replace 'service_manager.stop "myapp_server"$',
-                "service_manager.stop 'myapp_server', :silent => true"
-            end or raise "couldn't edit uninstaller"
-            ### INTERPRETER.cp recipe, "/tmp/uninstaller.rb"
-
-            AutomateIt::invoke(recipe, :project => project, :verbosity => Logger::WARN)
+        INTERPRETER.instance_eval do
+          log.silence(Logger::WARN) do
+            sh("#{ruby} bin/automateit #{params[:uninstaller]} > /dev/null 2>&1")
             File.exists?("/etc/init.d/myapp_server").should be_false
             File.directory?("/tmp/myapp_server").should be_false
-            INTERPRETER.service_manager.stopped?("myapp_server", :wait => 5).should be_true
+            service_manager.stopped?("myapp_server", :wait => 5).should be_true
           end
         end
       end
---
+++ spec/integration/examples_spec_editor.rb
@@ -0,0 +1,69 @@
+require File.join(File.dirname(File.expand_path(__FILE__)), "/../spec_helper.rb")
+
+# This spec edits the recipes to make them run silently when used with an internal invoke. Although this is faster and more portable, it's also evil and brittle because it can't run the recipes as they are.
+
+if not INTERPRETER.superuser?
+  puts "NOTE: Must be root to check #{__FILE__}"
+else
+  describe "Examples" do
+    params = {
+      :project => "examples/basic",
+      :installer => "examples/basic/recipes/install.rb",
+      :uninstaller => "examples/basic/recipes/uninstall.rb",
+    }
+    INTERPRETER.params = params
+
+    begin
+      # This will throw a NotImplemented error on unsupported platforms
+      AutomateIt::invoke(params[:installer], :verbosity => Logger::WARN, :noop => true)
+
+      it "should install the example" do
+        INTERPRETER.instance_eval do
+          mktemp do |recipe|
+            log.silence(Logger::WARN) do
+              cp(params[:installer], recipe)
+              edit(:file => recipe) do
+                replace 'service_manager.start\("myapp_server"\)$',
+                  'service_manager.start "myapp_server", :silent => true'
+                replace 'sh\("rails --database=sqlite3 . > /dev/null"\)',
+                  'sh("rails --database=sqlite3 . > /dev/null 2>&1")'
+                replace 'sh\("rake db:migrate"\)',
+                  'sh("rake db:migrate > /dev/null 2>&1")'
+              end or raise "couldn't edit installer"
+              # cp recipe, "/tmp/installer.rb"
+
+              AutomateIt::invoke(recipe, :project => params[:project],
+                :verbosity => Logger::WARN)
+              File.exists?("/etc/init.d/myapp_server").should be_true
+              File.directory?("/tmp/myapp_server").should be_true
+              service_manager.started?("myapp_server", :wait => 5).should be_true
+            end
+          end
+        end
+      end
+
+      it "should uninstall the example" do
+        INTERPRETER.instance_eval do
+          mktemp do |recipe|
+            log.silence(Logger::WARN) do
+              cp(params[:uninstaller], recipe)
+              edit(:file => recipe) do
+                replace 'service_manager.stop "myapp_server"$',
+                  'service_manager.stop "myapp_server", :silent => true'
+              end or raise "couldn't edit uninstaller"
+              ### cp recipe, "/tmp/uninstaller.rb"
+
+              AutomateIt::invoke(recipe, :project => params[:project],
+                :verbosity => Logger::WARN)
+              File.exists?("/etc/init.d/myapp_server").should be_false
+              File.directory?("/tmp/myapp_server").should be_false
+              service_manager.stopped?("myapp_server", :wait => 5).should be_true
+            end
+          end
+        end
+      end
+    rescue NotImplementedError
+      puts "NOTE: Can't check examples on this platform, #{__FILE__}"
+    end
+  end
+end
