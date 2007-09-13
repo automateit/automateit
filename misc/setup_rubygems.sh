27529baf890996c9a399c048cf051322d1881833
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-13 06:32:54 -0700

    Reworked misc/setup_rubygems.sh so it works on FreeBSD, which lacks various commands.

--- misc/setup_rubygems.sh
+++ misc/setup_rubygems.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # RELEASES: http://rubyforge.org/frs/?group_id=126
 
-#PACKAGE="rubygems-0.9.0"
-#URL="http://rubyforge.org/frs/download.php/11289/rubygems-0.9.0.tgz"
 PACKAGE="rubygems-0.9.3"
 URL="http://rubyforge.org/frs/download.php/20585/rubygems-0.9.3.tgz"
 
-CACHE=`mktemp -p /tmp -d install_rubygems.XXXXXXXXXX`
-cd "$CACHE"
+pushd "/tmp"
+
+CACHE=`mktemp -d install_rubygems.XXXXXXXXXX`
+pushd "$CACHE"
 
 wget -c -T20 -q "$URL"
 tar xfz "$PACKAGE.tgz"
 cd "$PACKAGE"
 sudo ruby setup.rb
 
-cd /
+popd
 rm -rf "$CACHE"
+
+popd
