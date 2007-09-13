93a9ba594d5c462b04a79db63b3da829ac8692c0
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-12 17:53:34 -0700

    Included Spec::Matchers into test's INTERPRETER

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 $LOAD_PATH << File.join(File.dirname(File.expand_path(__FILE__)), "/../lib")
 
+# Remove environmental variables that will contaminate tests
 ENV.delete("AUTOMATEIT_PROJECT")
 
 require 'automateit'
 
+# Create a global instance
 INTERPRETER = AutomateIt.new unless defined?(INTERPRETER)
+
+# Inject matchers into interpreter, e.g., 'should be_true'
+INTERPRETER.class.send(:include, Spec::Matchers)
