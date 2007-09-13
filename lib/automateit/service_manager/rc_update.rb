1bc73f0f3a01e684451bd09c6754f54e4230cf96
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-13 00:43:33 -0700

    Added ServiceManager::RC_Update for Gentoo.

--- lib/automateit/service_manager/rc_update.rb
+++ lib/automateit/service_manager/rc_update.rb
@@ -3,10 +3,7 @@
 # RC_Update implements the #enabled?, #enable and #disable features of the
 # ServiceManager on Gentoo-like systems.
 class AutomateIt::ServiceManager::RC_Update < AutomateIt::ServiceManager::SYSV
-  # TODO ServiceManager::RC_Update -- implement
-
-  # XXX Commands in the comments are merely hints at how to do this.
-  ### depends_on :programs => %w(rc-update)
+  depends_on :programs => %w(rc-update)
 
   def suitability(method, *args) # :nodoc:
     return available? ? 2 : 0
@@ -15,24 +12,26 @@ class AutomateIt::ServiceManager::RC_Update < AutomateIt::ServiceManager::SYSV
   # See ServiceManager#enabled?
   def enabled?(service)
     _raise_unless_available
-    # Do NOT use Gentoo's rc-update because the idiot that wrote that
-    # utility truncates service names to look "prettier" and provides no
-    # way to disable this annoyance for people that need to query services
-    # by name.
-    #
-    #GENTOO_RUNLEVELS = %w(boot default)
-    #! GENTOO_RUNLEVELS.select{|runlevel| File.exists?(File.join("/etc/runlevels", runlevel, service))}.empty?
+    # Do NOT use Gentoo's rc-update because the idiot that wrote that utility
+    # truncates service names to look "prettier" and provides no way to disable
+    # this annoyance for people that need to query services by name.
+    result = %w(boot default).select do |runlevel|
+      File.exists?(File.join("/etc/runlevels", runlevel, service))
+    end
+    return ! result.empty?
   end
 
   # See ServiceManager#enable
   def enable(service, opts={})
     _raise_unless_available
-    #system("rc-update add #{service} default")
+    return false if enabled?(service)
+    interpreter.sh("rc-update add #{service} default > /dev/null 2>&1")
   end
 
   # See ServiceManager#disable
   def disable(service, opts={})
     _raise_unless_available
-    #system "rc-update del #{service} default"
+    return false unless enabled?(service)
+    interpreter.sh("rc-update del #{service} default > /dev/null 2>&1")
   end
 end
