c01a1dab2083be899acd142f7c6a6f93efdbfa20
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-13 06:10:44 -0700

    Added PlatformManager::FreeBSD

---
+++ lib/automateit/platform_manager/freebsd.rb
@@ -0,0 +1,25 @@
+# == PlatformManager::FreeBSD
+#
+# A PlatformManager driver for FreeBSD.
+class AutomateIt::PlatformManager::FreeBSD < AutomateIt::PlatformManager::Uname
+  depends_on :files => %w(/etc/portsnap.conf /etc/rc.conf)
+
+  def suitability(method, *args) # :nodoc:
+    # Must be higher than PlatformManager::Struct
+    return available? ? 3 : 0
+  end
+
+  def _prepare
+    return if @struct[:distro]
+    @struct[:distro] = "freebsd"
+    @struct[:release] = `uname -r`.strip.match(/^([\d\.]+)-/)[1]
+    @struct
+  end
+  private :_prepare
+
+  def query(search)
+    _prepare
+    super(search)
+  end
+end
+
