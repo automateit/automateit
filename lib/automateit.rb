544b4c69cc37289a768104f237fdffe535dbc285
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-16 01:17:52 -0700

    Added require statements for HelpfulERB, NestedError and AutomateIt::Error.

--- lib/automateit.rb
+++ lib/automateit.rb
@@ -36,19 +36,22 @@ Hash.module_eval{include ActiveSupport::CoreExtensions::Hash::Keys}
 require 'ext/object.rb'
 require 'ext/metaclass.rb'
 
+# Helpers
+require 'hashcache'
+require 'queued_logger'
+require 'tempster'
+require 'helpful_erb'
+require 'nested_error'
+
 # Core
 require 'automateit/root'
 require 'automateit/constants'
+require 'automateit/error'
 require 'automateit/common'
 require 'automateit/interpreter'
 require 'automateit/plugin'
 require 'automateit/cli'
 require 'automateit/project'
-
-# Helpers
-require 'hashcache'
-require 'queued_logger'
-require 'tempster'
 
 # Plugins which must be loaded early
 require 'automateit/shell_manager'
