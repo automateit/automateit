677401a0628fe01ee271d7a7862e975bbe60eecc
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-17 19:07:30 -0700

    Reworked setup_gem_dependencies with to include debug and breakpoint

--- misc/setup_gem_dependencies.sh
+++ misc/setup_gem_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cmd = 'gem install -y rake open4 activesupport rspec ruby-breakpoint ruby-debug --no-rdoc --no-ri'
+
 # Run it twice because the first one will usually fail. Oh how I hate gem. :/
-gem install -y rake open4 activesupport rspec --no-rdoc --no-ri \
-    || gem install -y rake open4 activesupport rspec --no-rdoc --no-ri
+$cmd
+$cmd
