be3293aff8f019c2c1f2321bfd0c8912ee5f1bd6
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-19 21:15:32 -0700

    Refactored cli_spec using helpers to eliminate redundancy and group similar examples together.

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -1,15 +1,6 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), "/../spec_helper.rb")
 
-describe "AutomateIt::CLI" do
-  it "should create a project" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-      File.directory?(project).should be_true
-      File.exists?(File.join(project, "config", "tags.yml")).should be_true
-    end
-  end
-
+describe AutomateIt::CLI, " stand-alone" do# {{{
   it "should eval code" do
     AutomateIt::CLI.run(:eval => "42").should == 42
   end
@@ -20,18 +11,184 @@ describe "AutomateIt::CLI" do
       AutomateIt::CLI.run(:recipe => filename).should == 42
     end
   end
+end# }}}
 
-  it "should invoke a project recipe that can access a dist directory" do
+describe AutomateIt::CLI, " without a project" do# {{{
+  def on_recipe(opts={})
     INTERPRETER.mktempdircd do
-      project = "myproject"
-      recipe = File.join(project, "recipes", "recipe.rb")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-      File.open(recipe, "w+") {|h| h.write("dist")}
-      path = AutomateIt::CLI.run(:recipe => recipe)
-      File.expand_path(path).should == File.expand_path(File.join(project, "dist"))
+      recipe = "recipe.rb"
+      File.open(recipe, "w+"){|h| h.write("42")}
+      args = [recipe]
+      args << opts unless opts.blank?
+      return AutomateIt::CLI.run(*args)
     end
   end
 
+  it "should run recipe with a guessed but non-existent project path" do
+    on_recipe().should == 42
+  end
+  it "should fail to run recipe with explicit but non-existent project path" do
+    lambda{ on_recipe(:project => "not_a_real_project") }.should raise_error(ArgumentError)
+  end
+end# }}}
+
+describe AutomateIt::CLI, " with a project" do# {{{
+  def with_project(&block)
+    INTERPRETER.mktempdircd do
+      project = "myproject"
+      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
+
+      INTERPRETER.cd project do
+        block.call(project)
+      end
+    end
+  end
+
+  it "should create a project" do
+    with_project do
+      File.exists?("config/tags.yml").should be_true
+    end
+  end
+
+  it "should invoke a project recipe that can access a dist directory" do
+    with_project do
+      recipe =  "recipes/recipe.rb"
+      write_to(recipe, "dist")
+
+      path = AutomateIt::CLI.run(:recipe => recipe)
+      File.expand_path(path).should == File.expand_path("./dist")
+    end
+  end
+
+  it "should load custom driver" do
+    with_project do
+      recipe = "recipes/recipe.rb"
+      driver = "lib/custom_driver.rb"
+
+      write_to(driver, <<-HERE)
+        class ::AutomateIt::PackageManager::MyDriver < ::AutomateIt::PackageManager::BaseDriver
+          depends_on :nothing
+
+          def suitability(method, *args) # :nodoc:
+            # Never select as default driver
+            return 0
+          end
+        end
+      HERE
+
+      write_to(recipe, <<-HERE)
+        package_manager.drivers.keys.include?(:my_driver)
+      HERE
+
+      AutomateIt::CLI.run(:recipe => recipe).should be_true
+    end
+  end
+
+  it "should load custom driver with a different namespace than its manager" do
+    with_project do
+      recipe = "recipes/recipe.rb"
+      driver = "lib/custom_driver.rb"
+
+      # Keep this in sync with rdoc example in: lib/automateit/plugins/driver.rb
+      write_to(driver, <<-HERE)
+        class MyOtherDriver < ::AutomateIt::PackageManager::BaseDriver
+          depends_on :nothing
+
+          def suitability(method, *args) # :nodoc:
+            # Never select as default driver
+            return 0
+          end
+        end
+      HERE
+
+      write_to(recipe, <<-HERE)
+        package_manager.drivers.keys.include?(:my_other_driver)
+      HERE
+
+      AutomateIt::CLI.run(:recipe => recipe).should be_true
+    end
+  end
+
+  it "should load tags in a project" do
+    with_project do
+      recipe = "recipes/recipe.rb"
+      tags_yml = "config/tags.yml"
+
+      write_to(tags_yml, <<-HERE)
+        all_servers:
+          - localhost
+        all_groups:
+          - @all_servers
+        no_servers:
+          - !localhost
+        no_groups:
+          - !@all_servers
+      HERE
+
+      write_to(recipe, <<-HERE)
+        result = true
+        result &= tagged?("localhost")
+        result &= tagged?("all_servers")
+        result &= tagged?("all_groups")
+        result &= ! tagged?("no_servers")
+        result &= ! tagged?("no_groups")
+
+        result
+      HERE
+
+      AutomateIt::CLI.run(:recipe => recipe).should be_true
+    end
+  end
+
+  it "should load fields in a project" do
+    with_project do
+      recipe = "recipes/recipe.rb"
+      fields = "config/fields.yml"
+
+      write_to(fields, <<-HERE)
+        <%="key"%>: value
+        hash:
+          leafkey: leafvalue
+          branchkey:
+            deepleafkey: deepleafvalue
+      HERE
+
+      write_to(recipe, <<-HERE)
+        lookup("*")
+        lookup("key")
+        lookup("hash")
+        lookup("hash")["leafkey"]
+        lookup("hash#leafkey")
+        lookup("hash#branchkey#deepleafkey")
+        lookup("asdf") rescue IndexError
+        true
+      HERE
+
+      AutomateIt::CLI.run(:recipe => recipe).should be_true
+    end
+  end
+
+  it "should let recipes invoke other recipes" do
+    with_project do
+      first = "recipes/first.rb"
+      second = "recipes/second.rb"
+
+      write_to(first, "invoke 'second'")
+      write_to(second, "42")
+
+      AutomateIt::CLI.run(first).should == 42
+    end
+  end
+
+  it "should be able to run default project" do
+    with_project do
+      output = `rake -I #{AutomateIt_Lib} preview hello`
+      output.should match(/I'm in preview mode/)
+    end
+  end
+end# }}}
+
+describe AutomateIt::CLI, " with an interactive shell" do# {{{
   it "should provide an interactive shell" do
     # FIXME this seems to break IRB somehow for breakpoints, why?!
     # Mock IRB to run shell and return it to unmocked state when done
@@ -53,171 +210,4 @@ describe "AutomateIt::CLI" do
       $irb_under_test = false
     end
   end
-
-  it "should load custom driver" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      recipe = File.join(project, "recipes", "recipe.rb")
-      driver = File.join(project, "lib", "custom_driver.rb")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      # Keep this in sync with rdoc example in: lib/automateit/plugins/driver.rb
-      File.open(driver, "w+"){|h| h.write(<<-HERE)}
-        class ::AutomateIt::PackageManager::MyDriver < ::AutomateIt::PackageManager::BaseDriver
-          depends_on :nothing
-
-          def suitability(method, *args) # :nodoc:
-            # Never select as default driver
-            return 0
-          end
-        end
-      HERE
-
-      File.open(recipe, "w+"){|h| h.write(<<-HERE)}
-        package_manager.drivers.keys.include?(:my_driver)
-      HERE
-
-      AutomateIt::CLI.run(:recipe => recipe).should be_true
-    end
-  end
-
-  it "should load custom driver that's in a different namespace from its manager" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      recipe = File.join(project, "recipes", "recipe.rb")
-      driver = File.join(project, "lib", "custom_driver.rb")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      # Keep this in sync with rdoc example in: lib/automateit/plugins/driver.rb
-      File.open(driver, "w+"){|h| h.write(<<-HERE)}
-        class MyOtherDriver < ::AutomateIt::PackageManager::BaseDriver
-          depends_on :nothing
-
-          def suitability(method, *args) # :nodoc:
-            # Never select as default driver
-            return 0
-          end
-        end
-      HERE
-
-      File.open(recipe, "w+"){|h| h.write(<<-HERE)}
-        package_manager.drivers.keys.include?(:my_other_driver)
-      HERE
-
-      AutomateIt::CLI.run(:recipe => recipe).should be_true
-    end
-  end
-
-  it "should load tags in a project" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      recipe = File.join(project, "recipes", "recipe.rb")
-      tags_yml = File.join(project, "config", "tags.yml")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      File.open(tags_yml, "w+"){|h| h.write(<<-HERE)}
-        all_servers:
-          - localhost
-        all_groups:
-          - @all_servers
-        no_servers:
-          - !localhost
-        no_groups:
-          - !@all_servers
-      HERE
-
-      File.open(recipe, "w+"){|h| h.write(<<-HERE)}
-        result = true
-        result &= tagged?("localhost")
-        result &= tagged?("all_servers")
-        result &= tagged?("all_groups")
-        result &= ! tagged?("no_servers")
-        result &= ! tagged?("no_groups")
-
-        result
-      HERE
-
-      AutomateIt::CLI.run(:recipe => recipe).should be_true
-    end
-  end
-
-  it "should load fields in a project" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      recipe = File.join(project, "recipes", "recipe.rb")
-      fields = File.join(project, "config", "fields.yml")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      File.open(fields, "w+"){|h| h.write(<<-HERE)}
-        <%="key"%>: value
-        hash:
-          leafkey: leafvalue
-          branchkey:
-            deepleafkey: deepleafvalue
-      HERE
-
-      File.open(recipe, "w+"){|h| h.write(<<-HERE)}
-        lookup("*")
-        lookup("key")
-        lookup("hash")
-        lookup("hash")["leafkey"]
-        lookup("hash#leafkey")
-        lookup("hash#branchkey#deepleafkey")
-        lookup("asdf") rescue IndexError
-        true
-      HERE
-
-      AutomateIt::CLI.run(:recipe => recipe).should be_true
-    end
-  end
-
-  it "should let recipes invoke other recipes" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      first = File.join(project, "recipes", "first.rb")
-      second = File.join(project, "recipes", "second.rb")
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      File.open(first, "w+"){|h| h.write(<<-HERE)}
-        invoke 'second'
-      HERE
-
-      File.open(second, "w+"){|h| h.write(<<-HERE)}
-        42
-      HERE
-
-      AutomateIt::CLI.run(first).should == 42
-    end
-  end
-
-  it "should be able to run default project" do
-    INTERPRETER.mktempdircd do
-      project = "myproject"
-      AutomateIt::CLI.run(:create => project, :verbosity => Logger::WARN)
-
-      INTERPRETER.cd project do
-        output = `rake -I #{AutomateIt_Lib} preview hello`
-        output.should match(/I'm in preview mode/)
-      end
-    end
-  end
-end
-
-describe AutomateIt::CLI, " without a project" do
-  def on_recipe(opts={})
-    INTERPRETER.mktempdircd do
-      recipe = "recipe.rb"
-      File.open(recipe, "w+"){|h| h.write("42")}
-      args = [recipe]
-      args << opts unless opts.blank?
-      return AutomateIt::CLI.run(*args)
-    end
-  end
-
-  it "should not fail to run a recipe with a non-existent guessed project path" do
-    on_recipe().should == 42
-  end
-  it "should fail to run a recipe with an non-existent but explicit project path" do
-    lambda{ on_recipe(:project => "not_a_real_project") }.should raise_error(ArgumentError)
-  end
-end
+end# }}}
