a6f7eeba5b8b708b4dbbcc5c098c6f38acf210d1
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-24 16:19:56 -0700

    Publishing 0.70923

--- lib/automateit/constants.rb
+++ lib/automateit/constants.rb
@@ -4,7 +4,7 @@ module AutomateIt # :nodoc:
   # Various constants.
   module AutomateIt::Constants
     # AutomateIt version
-    VERSION=Gem::Version.new("0.70922")
+    VERSION=Gem::Version.new("0.70923")
 
     # Output prefix for command execution, e.g., "** ls -la"
     PEXEC = "** "
