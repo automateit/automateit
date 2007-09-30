5d419be0ab8a6eea9f353e0862160aa0f12cbbf1
Igal Koshevoy <igal@pragmaticraft.com>
2007-09-29 20:43:17 -0700

    Refactored example install recipe for greater clarity.

--- examples/basic/recipes/install.rb
+++ examples/basic/recipes/install.rb
@@ -1,31 +1,38 @@
-# Install packages on hosts tagged 'rails_servers' or 'myapp_servers'
+# Install dependencies on hosts with 'rails_servers' or 'myapp_servers' roles
 if tagged?("rails_servers | myapp_servers")
-  # Install platform-specific packages, queries system-provided tags
+  # Install platform-specific packages
   if tagged?("ubuntu | debian")
-    package_manager.install(%w(build-essential ruby1.8-dev libsqlite3-dev))
+    # Install the 'build-essential' package and others on Ubuntu or Debian
+    package_manager.install("build-essential", "ruby1.8-dev", "libsqlite3-dev")
   elsif tagged?("fedoracore | fedora | centos")
-    package_manager.install(%w(gcc ruby-devel sqlite-devel))
+    # Install equivalent packages on Fedora and similar OSes
+    package_manager.install("gcc", "'ruby-devel", "sqlite-devel")
   else
+    # Fail if running on another platform
     raise NotImplementedError.new("no packages specified for this platform")
   end
 
-  # Install Gems
-  package_manager.install(%w(rails sqlite3-ruby mongrel),
+  # Install Rails and supporting libraries with RubyGems
+  package_manager.install("rails", "sqlite3-ruby", "mongrel",
     :with => :gem, :docs => false)
-end
+end # ENDS: if tagged?("rails_servers | myapp_servers")
 
-# Setup the myapp server
+# Setup the myapp server, a simple Rails server instance
 if tagged?(:myapp_servers)
   # Create user for the application
   account_manager.add_user(lookup(:user))
 
-  # Create a directory for the application
+  # Create a directory for the application and 'cd' into it
   mkdir_p(lookup(:path)) do
     # Run shell commands to create the app and database
-    sh("rails --database=sqlite3 . > /dev/null") \
-      unless File.exists?("config/routes.rb")
+    unless File.exists?("config/routes.rb")
+      sh("rails --database=sqlite3 . > /dev/null")
+    end
 
-    sh("rake db:migrate") if Dir["db/*.sqlite3"].empty?
+    # Create the database if it doesn't exist.
+    if Dir["db/*.sqlite3"].empty?
+      sh("rake db:migrate")
+    end
 
     # Edit the homepage
     edit(:file => "public/index.html") do
@@ -33,21 +40,22 @@ if tagged?(:myapp_servers)
       replace("Welcome aboard", "This is MyAppServer")
     end
 
-    # Change the ownership for the files
-    chown_R(lookup(:user), nil, ".")
+    # Set the ownership of the created files
+    chperm(".", :user => lookup(:user), :recurse => true)
 
-    # Generate a service startup file from a template
-    render(:file => dist+"myapp_server.erb",
-           :to => "/etc/init.d/myapp_server",
-           :mode => 0555,
-           :locals => {
-              :path => lookup(:path),
-              :user => lookup(:user),
-              :port => lookup(:port),
-           }
+    # Generate a service startup file using a template
+    render(
+      :file => dist+"myapp_server.erb",
+      :to => "/etc/init.d/myapp_server",
+      :mode => 0555,
+      :locals => {
+        :path => lookup(:path),
+        :user => lookup(:user),
+        :port => lookup(:port),
+      }
     )
 
-    # Start the service
+    # Start the server
     service_manager.start("myapp_server")
-  end
-end
+  end # ENDS: mkdir_p(lookup(:path)) do
+end # ENDS: if tagged?(:myapp_servers)
