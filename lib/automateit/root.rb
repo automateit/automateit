f0493a55eccc00a7155ef0927fac621b631d0314
Igal Koshevoy <igal@pragmaticraft.com>
2007-10-01 16:05:14 -0700

    Publishing 0.70930

--- lib/automateit/root.rb
+++ lib/automateit/root.rb
@@ -1,7 +1,7 @@
 # See AutomateIt::Interpreter for usage information.
 module AutomateIt # :nodoc:
   # AutomateIt version
-  VERSION=Gem::Version.new("0.70928")
+  VERSION=Gem::Version.new("0.70930")
 
   # Instantiates a new Interpreter. See documentation for
   # Interpreter#setup.
