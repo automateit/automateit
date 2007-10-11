ff844a6c6296f4b77ce7e6230b9b9200783908f8
Igal Koshevoy <igal@pragmaticraft.com>
2007-10-11 00:53:47 -0700

    Added PlatformManager::SunOS

---
+++ lib/automateit/platform_manager/sunos.rb
@@ -0,0 +1,35 @@
+# == PlatformManager::SunOS
+#
+# A PlatformManager driver for SunOS.
+class AutomateIt::PlatformManager::SunOS < AutomateIt::PlatformManager::Uname
+  depends_on \
+    :programs => %w(uname),
+    :callbacks => [lambda {
+      begin
+        not `uname -s`.match(/SunOS/i).nil?
+      rescue
+        false
+      end
+    }]
+
+  def suitability(method, *args) # :nodoc:
+    # Must be higher than PlatformManager::Struct and Uname
+    return available? ? 3 : 0
+  end
+
+  def _prepare
+    return if @struct[:release]
+    @struct[:distro] = "sun"
+    @struct[:release] = `uname -r`.strip.downcase
+    @struct
+  end
+
+  def query(search)
+    _prepare
+    super(search)
+  end
+
+  def single_vendor?
+    return true
+  end
+end
