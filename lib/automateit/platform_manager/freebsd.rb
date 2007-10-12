1e33e6e635f355c43c4acefc33c570c3ba6012d3
Igal Koshevoy <igal@pragmaticraft.com>
2007-10-12 00:24:52 -0700

    Set PlatformManager::FreeBSD token to :freebsd, was :free_bsd

--- lib/automateit/platform_manager/freebsd.rb
+++ lib/automateit/platform_manager/freebsd.rb
@@ -2,6 +2,10 @@
 #
 # A PlatformManager driver for FreeBSD.
 class AutomateIt::PlatformManager::FreeBSD < AutomateIt::PlatformManager::Uname
+  def self.token
+    :freebsd
+  end
+
   depends_on :files => %w(/etc/portsnap.conf /etc/rc.conf)
 
   def suitability(method, *args) # :nodoc:
