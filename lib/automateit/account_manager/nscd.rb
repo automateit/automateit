67ee53a1bf1baa4cd9bed7f33d3c210b48596e44
Igal Koshevoy <igal@pragmaticraft.com>
2007-10-13 03:50:12 -0700

    UNSTABLE - Added callback for NSCD to check for daemon

--- lib/automateit/account_manager/nscd.rb
+++ lib/automateit/account_manager/nscd.rb
@@ -3,7 +3,8 @@
 # AccountManager driver for invalidating records stored in the NSCD, Name
 # Service Cache Daemon, found on Unix-like systems.
 class ::AutomateIt::AccountManager::NSCD < ::AutomateIt::AccountManager::BaseDriver
-  depends_on :programs => %w(nscd)
+  depends_on :programs => %w(nscd ps), 
+    :callbacks => [lambda{`ps -ef`.match(%r{/usr/sbin/nscd$})}]
 
   def suitability(method, *args) # :nodoc:
     # Level must be higher than Portable
