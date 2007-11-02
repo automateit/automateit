1946f00afc6f9544203f8eb42ffd340e13254cf8
Igal Koshevoy <igal@pragmaticraft.com>
2007-11-02 02:26:47 -0700

    Fixed misc helper for bootstrapping gem dependencies

--- misc/setup_gem_dependencies.sh
+++ misc/setup_gem_dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cmd = 'gem install -y rake open4 activesupport rspec ruby-breakpoint ruby-debug --no-rdoc --no-ri'
+cmd='sudo gem install -y rake open4 activesupport rspec ruby-breakpoint ruby-debug --no-rdoc --no-ri'
 
 # Run it twice because the first one will usually fail. Oh how I hate gem. :/
-$cmd
-$cmd
+$cmd || $cmd
