b44a63617199127f230db1838e5fcd45ae6aae39
Igal Koshevoy <igal@pragmaticraft.com>
2007-11-11 07:40:34 -0800

    Removed misc copy of which.cmd

--- misc/which.cmd
+++
@@ -1,6 +0,0 @@
-@ rem "which" workalike for Windows. Searches current directory and PATH for all extensions, stops after first match, and returns meaningful exit value for "system" or "ERRORLEVEL" checks.
-@setlocal
-@set P2=.;%PATH%
-@for %%e in (%PATHEXT%) do @for %%i in (%1%%e) do @if NOT "%%~$P2:i"=="" echo %%~$P2:i && goto end
-@exit 1
-:end
