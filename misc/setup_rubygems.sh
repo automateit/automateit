319262616d9b3d9d4b6334a8e6dcb2aade157e5b
Igal Koshevoy <igal@pragmaticraft.com>
2008-01-17 01:55:23 -0800

    Updated misc/setup_rubygems.sh, a simple shell script that downloads and install RubyGems 1.0.1, useful for bootstrapping a system to get AutomateIt running on it.

--- misc/setup_rubygems.sh
+++ misc/setup_rubygems.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
+# SUMMARY: Installs the rubygems system for managing Ruby libraries
+
 # RELEASES: http://rubyforge.org/frs/?group_id=126
 
-PACKAGE="rubygems-0.9.3"
-URL="http://rubyforge.org/frs/download.php/20585/rubygems-0.9.3.tgz"
+URL="http://rubyforge.org/frs/download.php/29548/rubygems-1.0.1.tgz"
+PACKAGE=$(echo $URL | sed "s/\.[^\.]*$//; s/^.*\///")
 
 pushd "/tmp"
-
-CACHE=`mktemp -d install_rubygems.XXXXXXXXXX`
-pushd "$CACHE"
-
-wget -c -T20 -q "$URL"
-tar xfz "$PACKAGE.tgz"
-cd "$PACKAGE"
-sudo ruby setup.rb
-
+  CACHE=`mktemp -d install_rubygems.XXXXXXXXXX`
+  pushd "$CACHE"
+    wget -c -T20 -q "$URL"
+    tar xfz "$PACKAGE.tgz"
+    cd "$PACKAGE"
+    sudo ruby setup.rb
+  popd
 popd
-rm -rf "$CACHE"
 
-popd
+# Uninstall, only run this if there's no other version of rubygems on the system or it'll destroy those up too:
+### rm -rf /usr/bin/{gem{,1.*},update_rubygems*} /usr/local/lib/site_ruby/1.8/{{,r}ubygems.rb,rubygems} /usr/lib/ruby/gems
