faf2c0db79994411b945dbe2f48ccf0487a730ac
Igal Koshevoy <igal@pragmaticraft.com>
2009-11-04 14:02:46 -0800

    Corrected download_spec, updated string it searches on at Google to confirm the download succeeded. Refactored for clarity.

--- spec/integration/download_spec.rb
+++ spec/integration/download_spec.rb
@@ -3,42 +3,41 @@ require File.join(File.dirname(File.expand_path(__FILE__)), "/../spec_helper.rb"
 describe "AutomateIt::DownloadManager" do
   before :all do
     @a = AutomateIt.new(:verbosity => Logger::WARN)
+    @source = "http://www.google.com/"
+    @match = /search/mi
   end
 
   it "should download a web page to a file" do
     @a.mktempdircd do
-      source = "http://www.google.com/"
       target = "google.html"
 
-      @a.download(source, :to => target).should be_true
+      @a.download(@source, :to => target).should be_true
 
       File.exists?(target).should be_true
-      File.read(target).should =~ /<html>.*Google/im
+      File.read(target).should =~ @match
     end
   end
 
-  it "should download a web page to a directory" do
+  it "should download a web page to a specific directory" do
     @a.mktempdircd do
-      source = "http://www.google.com/"
-      intermediate = "."
-      target = "www.google.com"
+      directory = "."
+      target = File.join(directory, "www.google.com")
 
-      @a.download(source, :to => intermediate).should be_true
+      @a.download(@source, :to => directory).should be_true
 
       File.exists?(target).should be_true
-      File.read(target).should =~ /<html>.*Google/im
+      File.read(target).should =~ @match
     end
   end
 
-  it "should download a web page to current directory" do
+  it "should download a web page by default to current directory" do
     @a.mktempdircd do
-      source = "http://www.google.com/"
       target = "www.google.com"
 
-      @a.download(source).should be_true
+      @a.download(@source).should be_true
 
       File.exists?(target).should be_true
-      File.read(target).should =~ /<html>.*Google/im
+      File.read(target).should =~ @match 
     end
   end
 end
